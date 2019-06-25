a4e0739ba3473c673b54e97072afeb431898cbb1
Gustavo Guichard <gustavoguichard@gmail.com>
2019-06-25 17:17:43 -0300

    Updates

---
+++ precache-manifest.51fc44a6f0152eaa96545f26bdb7bdfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53cdafad7c34d376f33a56fc29e21db8",
+    "url": "/wealthy-text-editor/index.html"
+  },
+  {
+    "revision": "3787250fa43c6e91e683",
+    "url": "/wealthy-text-editor/static/css/main.bd36a7d7.chunk.css"
+  },
+  {
+    "revision": "56391bdddca759025d43",
+    "url": "/wealthy-text-editor/static/js/2.dd9dc47b.chunk.js"
+  },
+  {
+    "revision": "3787250fa43c6e91e683",
+    "url": "/wealthy-text-editor/static/js/main.bf8f08bc.chunk.js"
+  },
+  {
+    "revision": "dfae548be16daf8b0fb2",
+    "url": "/wealthy-text-editor/static/js/runtime~main.7076fc43.js"
+  }
+]);
