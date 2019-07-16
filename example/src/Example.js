31b72db35c4092a8a4ddd7039915fdadb8d0299b
Gustavo Guichard <gustavoguichard@gmail.com>
2019-07-16 15:39:08 -0300

    Uploaded images with uploods

--- example/src/Example.js
+++ example/src/Example.js
@@ -1,57 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import WealthyEditor from 'wealthy-text-editor'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism'
 import { Card, CardHeader, CardContent } from '@material-ui/core'
 
-const fileToBase64 = async file => {
-  const reader = new FileReader()
-  return new Promise((resolve, reject) => {
-    reader.onerror = () => {
-      reader.abort()
-      reject(new DOMException('Problem parsing input file.'))
-    }
-    reader.onloadend = () => {
-      resolve(reader.result)
-    }
-    reader.readAsDataURL(file)
+const Example = () => {
+  const [config, setConfig] = useState({
+    apiKey: process.env.REACT_APP_API_KEY,
+    storageBucket: process.env.REACT_APP_BUCKET,
   })
-}
+  useEffect(() => {
+    if (!config.apiKey || !config.storageBucket) {
+      const apiKey = prompt('Please enter your Firebase API key')
+      const storageBucket = prompt('Please enter your Storage Bucket')
+      setConfig({ apiKey, storageBucket })
+    }
+  }, [config.apiKey, config.storageBucket])
 
-const handleImageUpload = async file => {
-  const img = await fileToBase64(file)
-  return new Promise(resolve =>
-    setTimeout(() => {
-      resolve(img)
-    }, 1600),
-  )
-}
-
-const Example = () => (
-  <Card elevation={4} style={{ overflow: 'visible' }}>
-    <CardHeader title="WealthyEditor sample" />
-    <CardContent>
-      <WealthyEditor
-        onImageUpload={handleImageUpload}
-        counter
-        onChange={console.log}
-        initialText={text}
-      />
-    </CardContent>
-    <SyntaxHighlighter language="javascript" style={prism}>
-      {`
+  return (
+    <Card elevation={4} style={{ overflow: 'visible' }}>
+      <CardHeader title="WealthyEditor sample" />
+      <CardContent>
+        <WealthyEditor
+          config={config}
+          uploodsMaxDimension={400}
+          uploodsOverwrite
+          counter
+          onChange={console.log}
+          initialText={text}
+        />
+      </CardContent>
+      <SyntaxHighlighter language="javascript" style={prism}>
+        {`
   import WealthyEditor from 'wealthy-text-editor'
 
   <WealthyEditor
-    onImageUpload={backendHandleImageUpload}
     counter
+    config={{ apiKey: 'MyFirebaseKey', storageBucket: 'MyBucket' }}
+    uploodsOverwrite
+    uploodsMaxDimension={400}
+    uploodsPrefix="some-folder/user-profiles"
     onChange={console.log}
     initialText="<h1>How a Rare..."
   />
               `}
-    </SyntaxHighlighter>
-  </Card>
-)
+      </SyntaxHighlighter>
+    </Card>
+  )
+}
 
 const text = `
 <h1>How a Rare Solar <em>‘Superflare’</em> Could Cripple Humanity</h1><h2>Our networked, electrified society makes us uniquely vulnerable to the effects of sudden solar weather</h2><p><br></p><p>Life on Earth wouldn’t be possible without the steady shine of our sun, but every now and again, it flares up, at times so strongly it disrupts cell phone calls, knocks a satellite or two silly, trips a power grid, even in one extreme case, starts fires. But in modern times at least, the sun hasn’t yet erupted in a <em>superflare</em> — the kind of colossal cosmic disturbance scientists have detected emanating from sun-like stars elsewhere in the galaxy.</p>
