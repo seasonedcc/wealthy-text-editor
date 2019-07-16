39926202c9a56d860462146dba30ea93f0260a6d
Gustavo Guichard <gustavoguichard@gmail.com>
2019-07-16 15:43:47 -0300

    Updates

---
+++ precache-manifest.40e36e9e70667a1f843109bdfc893c13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e518d54dc387ba7221a8d1d01d936e6",
+    "url": "/wealthy-text-editor/index.html"
+  },
+  {
+    "revision": "73386cddaae5223e20f2",
+    "url": "/wealthy-text-editor/static/css/main.87b1331b.chunk.css"
+  },
+  {
+    "revision": "283418023c18bee0e578",
+    "url": "/wealthy-text-editor/static/js/2.87d71178.chunk.js"
+  },
+  {
+    "revision": "73386cddaae5223e20f2",
+    "url": "/wealthy-text-editor/static/js/main.a62a114c.chunk.js"
+  },
+  {
+    "revision": "dfae548be16daf8b0fb2",
+    "url": "/wealthy-text-editor/static/js/runtime~main.7076fc43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wealthy-text-editor/precache-manifest.293ea94dfb279022034d797c912d52fe.js"
+  "/wealthy-text-editor/precache-manifest.40e36e9e70667a1f843109bdfc893c13.js"
 );
 
 self.addEventListener('message', (event) => {
